e9e09874a90d817d8606d649bad157147636ef92
Keith Cirkel <keithamus@users.noreply.github.com>
2019-11-13 15:41:32 +0000

    fix: do not recompress already compressed files
    Fixes #4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import fs from 'fs'
 import {join, dirname} from 'path'
 import {createBrotliCompress} from 'zlib'
 
+const magicBytes = [
+  [0x1F, 0x8B], // .gz
+  [0xFD, 0x37, 0x7A, 0x58, 0x5A, 0x00, 0x00], // .xz
+  [0x04, 0x22, 0x4D, 0x18], // .lz2
+  [0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C] // .7z
+]
+
+function isCompressed (bundle) {
+  if (/\.(gz|zip|xz|lz2|7z)$/.test(bundle.fileName)) return true
+  for (const bytes of magicBytes) {
+    let matches = true
+    const sourceBytes = bundle.type === 'asset' ? bundle.source : Buffer.from(bundle.code)
+    for (let i = 0; i < bytes.length; ++i) {
+      matches = matches && bytes[0] === sourceBytes[0]
+    }
+    if (matches) return true
+  }
+  return false
+}
+
 function brotliCompressFile(file, options, minSize) {
   return new Promise(resolve => {
     fs.stat(file, (err, stats) => {
@@ -37,7 +57,8 @@ export default function brotli(options = {}) {
     },
     writeBundle: async bundle => {
       const compressCollection = []
-      const files = [...options.additional, ...Object.keys(bundle).map(f => join(_dir, f))]
+      const bundlesToCompress = Object.keys(bundle).filter(file => !isCompressed(bundle[file]))
+      const files = [...options.additional, ...bundlesToCompress.map(f => join(_dir, f))]
       for (const file of files) {
         compressCollection.push(brotliCompressFile(file, options.options, options.minSize))
       }
--- test/index.test.js
+++ test/index.test.js
@@ -6,10 +6,11 @@ const rimraf = require('rimraf')
 const {rollup} = require('rollup')
 const brotli = require('../lib/index.cjs')
 const {promisify} = require('util')
-const {writeFile, readFile, access} = require('fs')
+const {writeFile, readFile, readdir, access} = require('fs')
 const readFileAsync = promisify(readFile)
 const writeFileAsync = promisify(writeFile)
 const rimrafAsync = promisify(rimraf)
+const readdirAsync = promisify(readdir)
 const accessAsync = promisify(access)
 const decompress = promisify(brotliDecompress)
 const {expect} = chai
@@ -83,6 +84,74 @@ describe('rollup-plugin-brotli', () => {
       access = null
     }
     expect(access).to.equal(null)
+  })
+
+  it('does not brotli up gzips', async () => {
+    mkdirSync('test/__output', {recursive: true, mode: 0o755})
+    const bundle = await rollup({
+      input: 'test/sample/index.js',
+      plugins: [
+        {
+          name: 'plugin1',
+          generateBundle(opts,bundle) {
+            bundle['test1.js.gz'] = {
+              fileName: 'test1.js.gz',
+              isAsset: true,
+              source: Buffer.from('hello world'),
+              type: 'asset'
+            }
+          }
+        },
+        brotli({ minSize: 0 })
+      ]
+    })
+    await bundle.write({
+      file: 'test/__output/bundle.js',
+      format: 'cjs',
+      sourceMap: true
+    })
+    const uncompressed = await readFileAsync('test/__output/bundle.js')
+    const compressed = await decompress(await readFileAsync('test/__output/bundle.js.br'))
+    expect(uncompressed).to.eql(compressed)
+    expect(await readdirAsync('test/__output')).to.eql([
+      'bundle.js',
+      'bundle.js.br',
+      'test1.js.gz',
+    ])
+  })
+
+  it('does not brotli up gzips (magic bytes)', async () => {
+    mkdirSync('test/__output', {recursive: true, mode: 0o755})
+    const bundle = await rollup({
+      input: 'test/sample/index.js',
+      plugins: [
+        {
+          name: 'plugin1',
+          generateBundle(opts,bundle) {
+            bundle['test1.js'] = {
+              fileName: 'test1.js',
+              isAsset: true,
+              source: Buffer.concat([Buffer.from([0x1F, 0x8B]), Buffer.from('hello world')]),
+              type: 'asset'
+            }
+          }
+        },
+        brotli({ minSize: 0 })
+      ]
+    })
+    await bundle.write({
+      file: 'test/__output/bundle.js',
+      format: 'cjs',
+      sourceMap: true
+    })
+    const uncompressed = await readFileAsync('test/__output/bundle.js')
+    const compressed = await decompress(await readFileAsync('test/__output/bundle.js.br'))
+    expect(uncompressed).to.eql(compressed)
+    expect(await readdirAsync('test/__output')).to.eql([
+      'bundle.js',
+      'bundle.js.br',
+      'test1.js',
+    ])
   })
 
 })
