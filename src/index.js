904c148e78ed8916f65dbbb1245c8df8f3b019e8
bompus <bompus@users.noreply.github.com>
2021-02-01 11:25:27 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import fs from 'fs'
 import {join, dirname} from 'path'
 import {createBrotliCompress} from 'zlib'
 
+const magicBytes = [
+  [0x1F, 0x8B], // .gz
+  [0xFD, 0x37, 0x7A, 0x58, 0x5A, 0x00, 0x00], // .xz
+  [0x04, 0x22, 0x4D, 0x18], // .lz2
+  [0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C] // .7z
+]
+
+function isCompressed (bundle) {
+  if (/\.(gz|zip|xz|lz2|7z|woff|woff2|jpg|jpeg|png|webp)$/.test(bundle.fileName)) return true
+  for (const bytes of magicBytes) {
+    let matches = true
+    const sourceBytes = bundle.type === 'asset' ? bundle.source : Buffer.from(bundle.code)
+    for (let i = 0; i < bytes.length; ++i) {
+      matches = matches && bytes[i] === sourceBytes[i]
+    }
+    if (matches) return true
+  }
+  return false
+}
+
 function brotliCompressFile(file, options, minSize) {
   return new Promise(resolve => {
     fs.stat(file, (err, stats) => {
@@ -38,7 +58,11 @@ export default function brotli(options = {}) {
     },
     writeBundle: async (outputOptions, bundle) => {
       const compressCollection = []
-      const bundlesToCompress = Object.keys(bundle).map(id => bundle[id].fileName).filter(fileName => options.test.test(fileName))
+      const bundlesToCompress = 
+        Object.keys(bundle)
+          .filter(id => options.test.test(bundle[id].fileName))
+          .filter(id => !isCompressed(bundle[id]))
+          .map(id => bundle[id].fileName)
       const files = [...options.additional, ...bundlesToCompress.map(f => join(_dir, f))]
       for (const file of files) {
         compressCollection.push(brotliCompressFile(file, options.options, options.minSize))
